5e58ffd223d9ca083e0322dd6f5c385afa1cb855
Stefan Pakic <turokdev@gmail.com>
2022-09-08 12:41:23 +0200

    Update and rename roi-calculator-v28.js to roi-calculator-v29.js

--- roi-calculator-v29.js
+++ roi-calculator-v29.js
@@ -432,7 +432,15 @@ jQuery(document).ready(function($) {
             $(".rate-from").text(data["percentageFrom"]);
             $(".rate-to").val(data["percentageTo"]);
             if (percentageToParam == 0) {
-                $(".rate-to").attr('data-min', 50);
+                let rateToMin = parseInt($('.rate-from').text());
+                let rateToCurrent = 51;
+
+                if(!isNaN(rateToMin))
+                {
+                  rateToCurrent = rateToMin + 1;
+                }
+
+                $(".rate-to").attr('data-min', rateToCurrent);
             }
             $(".roi_leads_to").text(data["leadsTo"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
             $(".roi_closed_deals").text(data["closedDeals"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
